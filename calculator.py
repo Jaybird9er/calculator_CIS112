51ea15991ecf9e058c4477d603219d84c18f9c4d
Jamey Kirk <jameykirk@gmail.com>
2021-05-19 17:52:32 +0900

    initial layout

--- calculator.py
+++ calculator.py
@@ -1,7 +1,25 @@
-
+import tkinter as tk
 
 # functions for buttons
+def digit1():
+    pass
 
 
 # calculator GUI
 
+calcWindow = tk.Tk()
+calcWindow.title("Calculator")
+
+## display frame and output display
+displayFrame = tk.Frame (calcWindow, height=50, width=400, bg="light blue")
+
+## button frame and buttons
+buttonFrame = tk.Frame (calcWindow, height=250, width=400, bg="#55BBDD")
+
+
+## layout frames and buttons
+displayFrame.pack()
+buttonFrame.pack()
+
+
+calcWindow.mainloop()
